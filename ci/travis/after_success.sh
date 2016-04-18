9b0e18b1cf47975b9ab9b1825a5d3552573c46a4
Georg Sauthoff <mail@georg.so>
2016-04-18 21:40:59 +0200

    Integrate Codecov.io CI service

---
+++ ci/travis/after_success.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -eux
+
+
+if [[ ! "$CMAKE_BUILD_TYPE" =~ "Coverage" ]]; then
+  exit 0
+fi
+
+
+function run_osx()
+{
+  bash <(curl -s https://codecov.io/bash) || echo "Codecov.io reporting failed"
+}
+
+function run_linux()
+{
+  :
+  # run in docker ...
+  #bash <(curl -s https://codecov.io/bash) -p
+}
+
+run_$TRAVIS_OS_NAME
