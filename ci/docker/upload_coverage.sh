3f5fd67101439649a82743cb823b7ac13dd4df50
Georg Sauthoff <mail@georg.so>
2016-04-20 21:32:48 +0200

    Workaround codecov not searching in build dir

--- ci/docker/upload_coverage.sh
+++ ci/docker/upload_coverage.sh
@@ -8,6 +8,7 @@ build_dir="$2"
 cd "$build_dir"
 
 # workaround coverage.cmake being identified as coverage report
-bash <(curl -s https://codecov.io/bash | sed '/-not -name .*coverage.txt/a -not -name '"'"'*.cmake'"'"' \\') \
+# workaround script only searching in the build directory for reports
+bash <(curl -s https://codecov.io/bash | sed '/-not -name .*coverage.txt/a -not -name '"'"'*.cmake'"'"' \\' | sed 's@files=.*find.*git_root.*coverage.*$@files=$(find "$proj_root" -type f \\( -name '"'"'*coverage.*'"'"' \\@' ) \
   -R "$src_dir" -p "$build_dir" \
   || echo "Codecov.io reporting failed"
