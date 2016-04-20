896cc0562116dd1b916f61152c769067a2065167
Georg Sauthoff <mail@georg.so>
2016-04-20 21:06:50 +0200

    Exclude *.cmake files from coverage report find

--- ci/docker/upload_coverage.sh
+++ ci/docker/upload_coverage.sh
@@ -7,6 +7,7 @@ build_dir="$2"
 
 cd "$build_dir"
 
-bash <(curl -s https://codecov.io/bash) \
+# workaround coverage.cmake being identified as coverage report
+bash <(curl -s https://codecov.io/bash | sed '/-not -name .*coverage.txt/a -not -name '"'"'*.cmake'"'"' \\') \
   -R "$src_dir" -p "$build_dir" \
   || echo "Codecov.io reporting failed"
