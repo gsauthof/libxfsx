933e212adbd5362b7ce73c8b1771e65f2caec228
Georg Sauthoff <mail@georg.so>
2016-04-21 23:46:01 +0200

    Add codecov workaround notes

--- ci/docker/upload_coverage.sh
+++ ci/docker/upload_coverage.sh
@@ -8,7 +8,14 @@ build_dir="$2"
 cd "$build_dir"
 
 # workaround coverage.cmake being identified as coverage report
-# workaround script only searching in the build directory for reports
-bash <(curl -s https://codecov.io/bash | sed '/-not -name .*coverage.txt/a -not -name '"'"'*.cmake'"'"' \\' | sed 's@files=.*find.*git_root.*coverage.*$@files=$(find "$proj_root" -type f \\( -name '"'"'*coverage.*'"'"' \\@' ) \
+# workaround script only searching in the source directory for gcov reports
+#
+# cf. https://github.com/codecov/codecov-bash/pull/29
+bash <(curl -s https://codecov.io/bash \
+  | sed '/-not -name .*coverage.txt/a -not -name '"'"'*.cmake'"'"' \\' \
+  | sed 's@files=.*find.*git_root.*coverage.*$@'\
+'files=$(find "$proj_root" -type f \\( -name '"'"'*coverage.*'"'"' \\@' ) \
+  \
   -R "$src_dir" -p "$build_dir" \
+  \
   || echo "Codecov.io reporting failed"
